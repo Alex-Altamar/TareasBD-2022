d6ca3363cc1b3c8ab1f2e8ee4b973c96b02c57d6
Alex-Altamar <aealtamar@uniguajira.edu.co>
2022-03-14 21:39:35 -0500

    agrego menu base + carpeta pi donde haremos las funciones a llamar

---
+++ Tarea3/Menu.py
@@ -0,0 +1,38 @@
+import os
+from api.library import*
+
+def menu():
+	"""
+	Función que limpia la pantalla y muestra nuevamente el menu
+	"""
+	os.system('cls') # NOTA para windows tienes que cambiar clear por cls
+	print ("Selecciona una opción")
+	print ("\t1 - Agregar Empleado... ")
+	print ("\t2 - Eliminar Empleado... ")
+	print ("\t3 - Mostrar opción")
+	print ("\t9 - Salir")
+
+while True:
+	# Mostramos el menu
+	menu()
+ 
+	# solicituamos una opción al usuario
+	opcionMenu = input("inserta un numero valor >> ")
+ 
+	if opcionMenu=="1":
+		print ("")
+		input("Has pulsado la opción 1...\npulsa una tecla para continuar")
+	elif opcionMenu=="2":
+		print ("")
+		input("Has pulsado la opción 2...\npulsa una tecla para continuar")
+	elif opcionMenu=="3":
+		print ("")
+		input("Has pulsado la opción 3...\npulsa una tecla para continuar")
+	elif opcionMenu=="9":
+		break
+	else:
+		print ("")
+		input("No has pulsado ninguna opción correcta...\npulsa una tecla para continuar")
+
+if __name__ == "__main__":
+    menu()
