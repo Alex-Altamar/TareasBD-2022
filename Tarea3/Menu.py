5a19d0bff3f20dd740b35089c6a19b79bc848e64
Alex-Altamar <aealtamar@uniguajira.edu.co>
2022-03-17 23:23:58 -0500

    agrego menu empleados

--- Tarea3/Menu.py
+++ Tarea3/Menu.py
@@ -1,38 +1,77 @@
-import os
+import sys
 from api.library import *
 
-def menu():
-	"""
-	Función que limpia la pantalla y muestra nuevamente el menu
-	"""
-	os.system('cls') # NOTA para windows tienes que cambiar clear por cls
-	print ("Selecciona una opción")
-	print ("\t1 - Agregar Empleado... ")
-	print ("\t2 - Eliminar Empleado... ")
-	print ("\t3 - Mostrar opción")
-	print ("\t9 - Salir")
+class Menu:
+    '''Muestra un menu y responde a elecciones cuando se ejecuta.'''
+    def __init__(self):
+        self.empleado = Empleado()
+        self.elecciones = {
+                "1" : self.add_empleado,
+                "2" : self.mostrar_empleados,
+                "3" : self.search_empleados,
+                "4" : self.modificar_empleado,
+                "5" : self.quit
+                } 
 
-while True:
-	# Mostramos el menu
-	menu()
- 
-	# solicituamos una opción al usuario
-	opcionMenu = input("inserta un numero valor >> ")
- 
-	if opcionMenu=="1":
-		print ("")
-		input("Has pulsado la opción 1...\npulsa una tecla para continuar")
-	elif opcionMenu=="2":
-		print ("")
-		input("Has pulsado la opción 2...\npulsa una tecla para continuar")
-	elif opcionMenu=="3":
-		print ("")
-		input("Has pulsado la opción 3...\npulsa una tecla para continuar")
-	elif opcionMenu=="9":
-		break
-	else:
-		print ("")
-		input("No has pulsado ninguna opción correcta...\npulsa una tecla para continuar")
+    def mostrar_menu(self):
+        print("""
+Menu Cuaderno
+
+1 Añadir Empleado
+2 Mostrar todos los empleados
+3 Buscar empleado
+4 Modificar empelado
+5 Salir
+""")
+
+    def run(self):
+        '''Muestra el menú y responde a las elecciones.'''
+        while True:
+            self.mostrar_menu()
+            eleccion = input("Escribe una opción: ")
+            accion = self.elecciones.get(eleccion)
+            if accion:
+                accion()
+            else:
+                print("{0} no es una elección válida".format(eleccion))
+
+    def mostrar_empleados(self, lista=None):
+        if not lista:
+            lista = self.empleado.lista
+        for persona in lista:
+            print("{0}: {1}\n{2}\n{3}\n{4}\n{5}\n{6}".format(persona.id, persona.cedula, persona.nombre, persona.apellido, persona.telefono, persona.direccion, persona.tags))
+
+    def search_empleados(self):
+        filter = input("Buscar por ID: ")
+        lista = self.empleado.search(filter)
+        self.mostrar_empleados(lista)
+
+    def add_empleado(self):
+        cedula=input("Ingrese numero de cedula: ")
+        nombre=input("Ingrese el nombre:   ")
+        apellido=input("Ingrese el apellido:  ")
+        direccion=input("Ingrese la direccion: ")
+        telefono=input("Ingrese el telefono:  ")
+        self.empleado.nuevo_empleado(Persona(cedula, nombre, apellido, telefono, direccion))
+
+        print("Tu empleado ha sido añadido.")
+
+    def modificar_empleado(self):
+        id = input("Escribe el id de un empleado: ")
+        cedula=input("Ingrese numero de cedula: ")
+        nombre=input("Ingrese el nombre:   ")
+        apellido=input("Ingrese el apellido:  ")
+        direccion=input("Ingrese la direccion: ")
+        telefono=input("Ingrese el telefono:  ")
+        tags = input("Escribe tags: ")
+        if Persona:
+            self.empleado.modificar_empleado(id, cedula, nombre, apellido, direccion, telefono, tags)
+        if tags:
+            self.empleado.modificar_tags(id, tags)
+
+    def quit(self):
+        print("Gracias por usar tu cuaderno hoy.")
+        sys.exit(0)
 
 if __name__ == "__main__":
-    menu()
+    Menu().run()
--- Tarea3/api/library.py
+++ Tarea3/api/library.py
@@ -1,1 +1,71 @@
+import datetime
 
+# Almacena todas las ids disponibles para las nuevas notas
+ultima_id = 0
+
+class Persona:
+    '''Representa una nota en el cuaderno. Se compara con un String
+    en las búsquedas y las etiquetas para cada nota'''
+    def __init__(self,tags=''):
+        '''inicializa una nota con memo y opcional tags
+        separado por comas. Automáticamente configura la fecha
+        de creación de la nota y una única id'''
+        self.cedula=input("Ingrese numero de cedula: ")
+        self.nombre=input("Ingrese el nombre:   ")
+        self.apellido=input("Ingrese el apellido:  ")
+        self.direccion=input("Ingrese la direccion: ")
+        self.telefono=input("Ingrese el telefono:  ")
+        self.tags = tags
+        self.creation_date = datetime.date.today()
+        global ultima_id
+        ultima_id += 1
+        self.id = ultima_id
+    def match(self, filter):
+        '''Determina si esta nota concuerda con el filtro
+        de texto. Devuelve True si concuerda, en otro caso False.
+        Búsqueda es case sensitive y compara tanto con text como
+        con tags.'''
+        return filter in self.cedula or filter in self.tags
+
+class Empleado:
+    def __init__(self):
+        super().__init__()
+    '''Representa una colección de notas que pueden ser etiquetadas,
+    modificadas, y se pueden buscar.'''
+
+    def __init__(self):
+        '''Inicialiaza lista pleado con una lista vacía.'''
+        self.lista = []
+
+    def nuevo_empleado(self, cedula, nombre, apellido, telefono, direccion, tags=''):
+        '''Crea un nuevo empleado y lo añade a la lista.'''
+        self.lista.append(Persona(cedula, nombre, apellido, telefono, direccion, tags))
+
+    def _encontrar_empleado(self, persona_id):
+        '''Localiza el empleado con la id dada.'''
+        for persona in self.lista:
+            if str (persona.id) == str(persona_id):
+                return persona
+        return None
+
+    def modificar_empleado(self, persona_id, cedula, nombre, apellido, telefono, direccion):
+        '''Encuentra la nota con la id dada y cambia al valor dado.'''
+        persona = self._encontrar_empleado(persona_id)
+        if persona:
+            persona.cedula, persona.nombre, persona.apellido, persona.telefono, persona.direccion =  cedula, nombre, apellido, telefono, direccion
+            return True
+        return False
+
+    def modificar_tags(self, persona_id, tags):
+        '''Encuentra la nota con la id dada y cambia sus
+        tags al valor dado.'''
+        persona = self._encontrar_persona(persona_id)
+        if persona:
+            persona.tags = tags
+            return True
+        return False
+    def search(self, filter):
+        '''Encuentra todas las notas que concuerdan con
+        el filtro string dado.'''
+        return [persona for persona in self.lista if
+                persona.match(filter)]
